96a51b9d575f38722f90d0d53dd21fb411cf3c61
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-09-29 09:04:07 +0200

    Worked On State In Cockpit.js
    Changed How State Is Updated i.e this.state(()=>{}) instead Of this.state({}) Because The State Update Depended On Previous State

--- Front-End/src/Components/Map/Cockpit.js
+++ Front-End/src/Components/Map/Cockpit.js
@@ -8,22 +8,25 @@ import ErrorBoundary from "../ErrorBoundary/ErrorBoundary";
 import Map from "./Map";
 
 class Cockpit extends React.Component {
-  state = {
-    address: "",
-    city: "",
-    area: "",
-    state: "",
-    zoom: 15,
-    height: 400,
-    mapPosition: {
-      lat: 0,
-      lng: 0,
-    },
-    markerPosition: {
-      lat: 0,
-      lng: 0,
-    },
-  };
+  constructor(props) {
+    super();
+    this.state = {
+      address: "",
+      city: "",
+      area: "",
+      state: "",
+      zoom: 15,
+      height: 400,
+      mapPosition: {
+        lat: 0,
+        lng: 0,
+      },
+      markerPosition: {
+        lat: 0,
+        lng: 0,
+      },
+    };
+  }
 
   componentDidMount() {
     try {
@@ -43,9 +46,10 @@ class Cockpit extends React.Component {
     try {
       const promise = handleComponentMount(lat, lng);
       promise.then((response) => {
-        const newState = Object.assign({}, this.state, response);
-
-        this.setState(newState);
+        this.setState((prevState, props) => {
+          const newState = Object.assign({}, prevState, response);
+          return newState;
+        });
       });
     } catch (error) {
       console.log(error);
@@ -56,9 +60,10 @@ class Cockpit extends React.Component {
     try {
       const promise = handleMarkerDragEnd(event);
       promise.then((response) => {
-        const newState = Object.assign({}, this.state, response);
-
-        this.setState(newState);
+        this.setState((prevState, props) => {
+          const newState = Object.assign({}, prevState, response);
+          return newState;
+        });
       });
     } catch (error) {
       console.log(error);
@@ -68,9 +73,10 @@ class Cockpit extends React.Component {
   onPlaceSelected = (place) => {
     try {
       const data = handlePlaceSelected(place);
-      const newState = Object.assign({}, this.state, data);
-
-      this.setState(newState);
+      this.setState((prevState, props) => {
+        const newState = Object.assign({}, prevState, data);
+        return newState;
+      });
     } catch (error) {
       console.log(error);
     }
