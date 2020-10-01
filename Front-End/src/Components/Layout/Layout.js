70d1b210d8e6de845b7457750ef5ac940d4c43db
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-10-01 20:09:53 +0200

    Layout Turned To Class

--- Front-End/src/Components/Layout/Layout.js
+++ Front-End/src/Components/Layout/Layout.js
@@ -1,9 +1,33 @@
 import React from "react";
+import ToolBar from "../Navigation/ToolBar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = (props) => (
-  <div data-test="component-layout">
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
-  </div>
-);
-export default layout;
+class Layout extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      showSideDrawer: false,
+    };
+  }
+  handleSideDrawerClose = () => {
+    this.setState({ showSideDrawer: false });
+  };
+  handleSideDrawerToggle = () => {
+    this.setState((prevState, prevProps) => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+  render() {
+    return (
+      <div data-test="component-layout">
+        <ToolBar drawerToggleClicked={this.handleSideDrawerToggle} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.handleSideDrawerClose}
+        />
+        <main>{this.props.children}</main>
+      </div>
+    );
+  }
+}
+export default Layout;
