2d784596497aec62119bc6d63ffc69cd855c1407
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-10-01 20:09:20 +0200

    Map tests and Map Css

--- Front-End/src/containers/__tests__/Map.test.js
+++ Front-End/src/containers/__tests__/Map.test.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
+import Enzyme, { mount, shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
+import ErrorBoundary from "../../components/ErrorBoundary/ErrorBoundary";
 import Map from "../Map/Map/";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
@@ -12,6 +13,8 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
  */
 const setup = () => shallow(<Map />);
 
+//const mountSetup = () => mount(<ErrorBoundary />);
+
 const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`);
 
 describe("MAP COMPONENT", () => {
@@ -21,14 +24,22 @@ describe("MAP COMPONENT", () => {
     expect(mapComponent.length).toBe(1);
   });
 
-  /*it("SHOULD HAVE AN ERROR BOUNDARY WRAPPING MAP COMPONENT", () => {
-    const wrapper = setup();
-    const errorComponent = findByTestAttr(wrapper, "component-error-boundary");
-    const errorBoundary = mapComponent.parent();
-    console.log(errorComponent.html());
-    work on Test
+  it("SHOULD HAVE AN ERROR BOUNDARY WRAPPING MAP COMPONENT", () => {
+    /* //Need To Mount A Class
+    const mapWrapper = setup();
+    const errorBoundaryWrapper = mountSetup();
+    const mapComponent = findByTestAttr(mapWrapper, "component-map");
+    const errorBoundaryComponent = findByTestAttr(
+      errorBoundaryWrapper,
+      "component-error-boundary"
+    );
+    console.log(errorBoundaryComponent.debug());
+    mapComponent.simulateError(new Error("HEY!"));
+    console.log(errorBoundaryComponent.debug());
+
+    
     expect(
       findByTestAttr(errorBoundary, "component-error-boundary").length
-    ).toBe(1);
-  });*/
+    ).toBe(1);*/
+  });
 });
