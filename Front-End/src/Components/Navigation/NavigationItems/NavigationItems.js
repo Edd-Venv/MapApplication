52d4e665be538f23b348b228f7edbbee910c14da
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-10-06 14:47:34 +0200

    SignIn Page Creation
    Also Passed Props To Close BackDrop To Nav Links

--- Front-End/src/Components/Navigation/NavigationItems/NavigationItems.js
+++ Front-End/src/Components/Navigation/NavigationItems/NavigationItems.js
@@ -2,26 +2,47 @@ import React from "react";
 import classes from "./NavigationItems.module.css";
 import NavigationItem from "./NavigationItem/NavigationItem";
 
-const navigationItems = () => (
+const navigationItems = (props) => (
   <ul
     className={classes.NavigationItems}
     data-test="component-navigation-items"
   >
-    <NavigationItem link="/" data-test="home-link">
+    <NavigationItem
+      link="/"
+      data-test="home-link"
+      closeBackDrop={props.closeBackDrop}
+    >
       Home{" "}
     </NavigationItem>
-    <NavigationItem link="/signin" data-test="sign-in-link">
+    <NavigationItem
+      link="/signin"
+      data-test="sign-in-link"
+      closeBackDrop={props.closeBackDrop}
+    >
       Sign In{" "}
     </NavigationItem>
-    <NavigationItem link="/signup" data-test="sign-up-link">
+    <NavigationItem
+      link="/signup"
+      data-test="sign-up-link"
+      closeBackDrop={props.closeBackDrop}
+    >
       Sign Up{" "}
     </NavigationItem>
-    <NavigationItem link="/mylocations" data-test="my-locations-link">
+    <NavigationItem
+      link="/mylocations"
+      data-test="my-locations-link"
+      closeBackDrop={props.closeBackDrop}
+    >
       My Locations
     </NavigationItem>
-    <NavigationItem link="/settings" data-test="settings-link">
+    <NavigationItem
+      link="/settings"
+      data-test="settings-link"
+      closeBackDrop={props.closeBackDrop}
+    >
       Settings{" "}
     </NavigationItem>
   </ul>
 );
+
 export default navigationItems;
--- Front-End/src/Components/UI/Form/Form.js
+++ Front-End/src/Components/UI/Form/Form.js
@@ -1,37 +1,58 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import classes from "./Form.module.css";
 
-const form = (props) => (
-  <form
-    className={classes.Form}
-    autoComplete="off"
-    onSubmit={props.handleSubmit}
-  >
-    <p>{props.formTitle}</p>
-    <div>
-      <label htmlFor={props.firstInputLabel} />
-      <input
-        name={props.firstInputLabel}
-        type={props.firstInputType}
-        value={props.firstInputValue}
-        onChange={props.handleChange}
-        ref={props.firstInputRef}
-      />
-    </div>
+const form = (props) => {
+  let forgotPassword = null;
 
-    <div>
-      <label htmlFor={props.secondInputLabel} />
-      <input
-        name={props.secondInputLabel}
-        type={props.secondInputType}
-        value={props.secondInputValue}
-        onChange={props.handleChange}
-        ref={props.secondInputRef}
-      />
-    </div>
-    <Button type={props.buttonType}>SignIn </Button>
-  </form>
-);
+  if (props.formTitle === "Sign In") {
+    forgotPassword = (
+      <Link to="/forgotPassword" style={{ textDecorationLine: "none" }}>
+        <p className={classes.ForgotPassword}>Forgot your password?</p>
+      </Link>
+    );
+  }
+
+  return (
+    <form
+      className={classes.Form}
+      autoComplete="off"
+      onSubmit={props.handleSubmit}
+    >
+      <h3>{props.formTitle}</h3>
+      <div>
+        <label htmlFor={props.firstInputLabel} />
+        <input
+          className={classes.Inputs}
+          name={props.firstInputLabel}
+          type={props.firstInputType}
+          value={props.firstInputValue}
+          onChange={props.handleChange}
+          ref={props.firstInputRef}
+          placeholder={props.firstInputPlaceHolder}
+        />
+      </div>
+
+      <div>
+        <label htmlFor={props.secondInputLabel} />
+        <input
+          className={classes.Inputs}
+          name={props.secondInputLabel}
+          type={props.secondInputType}
+          value={props.secondInputValue}
+          onChange={props.handleChange}
+          ref={props.secondInputRef}
+          placeholder={props.secondInputPlaceHolder}
+        />
+      </div>
+      {forgotPassword}
+      <div>
+        {" "}
+        <Button type={props.buttonType}>SignIn </Button>
+      </div>
+    </form>
+  );
+};
 
 export default form;
