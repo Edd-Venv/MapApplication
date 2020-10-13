a3d0eb03fc2717632131ff5b83ef61064fd8bc91
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-10-13 11:36:53 +0200

    General Updates

--- Front-End/src/containers/Map/Map.js
+++ Front-End/src/containers/Map/Map.js
@@ -2,15 +2,14 @@ import {
   GoogleMap,
   InfoWindow,
   Marker,
-  withGoogleMap,
-  withScriptjs,
-} from "react-google-maps";
+  useLoadScript,
+} from "@react-google-maps/api";
 import AutoComplete from "react-google-autocomplete";
 import PropTypes from "prop-types";
-import React from "react";
-import classes from "./Map.module.css";
+import React, { useRef } from "react";
 import mapStyle from "./MapStyle";
-import SaveButton from "../../components/SaveButton/SaveButton";
+import Button from "../../components/UI/Button/Button";
+import LocateMe from "../../components/UI/Locate/Locate";
 
 const InputContainer = { display: "flex", justifyContent: "center" };
 const style = {
@@ -34,133 +33,121 @@ const options = {
   zoomControl: true,
   styles: mapStyle,
 };
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
 
-class Map extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      zoom: 12,
-      panTo: { lat: 0, lng: 0 },
-    };
+const Map = (props) => {
+  const locations = props.state.myLocations;
+
+  const currentLocationMarkerRef = useRef(null);
+
+  let usersInforWindow = null;
+  let savedLocations = null;
+  let savedLocationInfoWindow = null;
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyCt6g43R5qohybxO911L1KQ_WwIsD6hX-8",
+    libraries,
+  });
+
+  if (loadError) return "Error Loading Map";
+  if (!isLoaded) return null;
+
+  if (props.state.showInfoWindow) {
+    usersInforWindow = (
+      <InfoWindow onCloseClick={() => props.onCloseUserInfoWindow()}>
+        <div>
+          <p>{props.state.address}</p>
+          <Button
+            buttonClick={() => props.onSaveLocation(props.state)}
+            type="button"
+          >
+            Save Location
+          </Button>
+        </div>
+      </InfoWindow>
+    );
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log("Should Update?");
-    //console.log(nextState.panTo, nextProps.state.panTo);
-    //if (nextProps.state.zoom !== nextState.zoom) return false;
-    //if (nextState.panTo.lat !== nextProps.state.panTo.lat) return false;
-    return true;
+  if (props.state.selectedMarkerData) {
+    savedLocationInfoWindow = (
+      <InfoWindow
+        position={props.state.selectedMarkerData.markerPosition}
+        onCloseClick={() => props.onCloseSelectedMarkerInfoWindow()}
+      >
+        <div>
+          <p>{props.state.selectedMarkerData.address}</p>
+          <Button buttonClick={() => {}} buttonType="button" className="Danger">
+            Delete Location
+          </Button>
+        </div>
+      </InfoWindow>
+    );
   }
 
-  render() {
-    const locations = this.props.state.myLocations;
-    let usersInforWindow = null;
-    let savedLocations = null;
-    let savedLocationInfoWindow = null;
+  if (locations.length > 0) {
+    savedLocations = locations.map((location) => {
+      let show = false;
 
-    if (this.props.state.showInfoWindow) {
-      usersInforWindow = (
-        <InfoWindow onCloseClick={() => this.props.onCloseUserInfoWindow()}>
-          <SaveButton
-            state={this.props.state}
-            onSaveLocation={this.props.onSaveLocation}
+      if (location.id && props.state.selectedMarkerData.id)
+        if (location.id === props.state.selectedMarkerData.id) show = true;
+
+      return (
+        <Marker
+          draggable={false}
+          key={location.id}
+          position={{
+            lat: location.markerPosition.lat,
+            lng: location.markerPosition.lng,
+          }}
+          onClick={() => props.onSelectedMarkerInfoWindow(location)}
+        >
+          {show ? savedLocationInfoWindow : null}
+        </Marker>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={13}
+        center={{
+          lat: props.state.mapPosition.lat,
+          lng: props.state.mapPosition.lng,
+        }}
+        options={options}
+      >
+        {savedLocations}
+        <Marker
+          draggable={true}
+          onDragEnd={props.onMarkerDragEnd}
+          position={{
+            lat: props.state.markerPosition.lat,
+            lng: props.state.markerPosition.lng,
+          }}
+          onClick={() => {
+            props.onShowUserInfoWindow();
+          }}
+          ref={currentLocationMarkerRef}
+        >
+          {usersInforWindow}
+        </Marker>
+        <div style={InputContainer}>
+          <AutoComplete
+            types={["(regions)"]}
+            onPlaceSelected={props.onPlaceSelected}
+            style={style}
           />
-        </InfoWindow>
-      );
-    }
-
-    if (this.props.state.selectedMarkerData) {
-      savedLocationInfoWindow = (
-        <InfoWindow
-          position={this.props.state.selectedMarkerData.markerPosition}
-          onCloseClick={() => this.props.onCloseSelectedMarkerInfoWindow()}
-        >
-          <p>{this.props.state.selectedMarkerData.address}</p>
-        </InfoWindow>
-      );
-    }
-
-    if (locations.length > 0) {
-      savedLocations = locations.map((location) => {
-        let show = false;
-
-        if (location.id && this.props.state.selectedMarkerData.id)
-          if (location.id === this.props.state.selectedMarkerData.id)
-            show = true;
-
-        return (
-          <Marker
-            key={location.id}
-            position={{
-              lat: location.markerPosition.lat,
-              lng: location.markerPosition.lng,
-            }}
-            onClick={() => this.props.onSelectedMarkerInfoWindow(location)}
-          >
-            {show ? savedLocationInfoWindow : null}
-          </Marker>
-        );
-      });
-    }
-
-    const GMap = withScriptjs(
-      withGoogleMap(() => (
-        <GoogleMap
-          onLoad={this.mapRef}
-          defaultZoom={this.state.zoom}
-          defaultCenter={{
-            lat: this.props.state.mapPosition.lat,
-            lng: this.props.state.mapPosition.lng,
-          }}
-          ref={(map) => {
-            this.mapRef = map;
-          }}
-          onZoomChanged={() => this.setState({ zoom: this.mapRef.getZoom() })}
-          options={options}
-        >
-          {savedLocations}
-          <Marker
-            draggable
-            onDragEnd={this.props.onMarkerDragEnd}
-            position={{
-              lat: this.props.state.markerPosition.lat,
-              lng: this.props.state.markerPosition.lng,
-            }}
-            onClick={() => {
-              this.props.onShowUserInfoWindow();
-            }}
-            onDrag={(event) => {
-              /* this.setState(
-                {
-                  panTo: { lat: event.latLng.lat(), lng: event.latLng.lng() },
-                },
-                this.mapRef.panTo(event.latLng)
-              ); */
-            }}
-          >
-            {usersInforWindow}
-          </Marker>
-          <div style={InputContainer}>
-            <AutoComplete
-              types={["(regions)"]}
-              onPlaceSelected={this.props.onPlaceSelected}
-              style={style}
-            />
-          </div>
-        </GoogleMap>
-      ))
-    );
-    return (
-      <GMap
-        data-test="component-map"
-        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCt6g43R5qohybxO911L1KQ_WwIsD6hX-8&v=3.exp&libraries=geometry,drawing,places"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div className={classes.map_container} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
-    );
-  }
-}
+        </div>
+        <LocateMe />
+      </GoogleMap>
+    </div>
+  );
+};
 Map.propTypes = {
   state: PropTypes.object.isRequired,
   onMarkerDragEnd: PropTypes.func.isRequired,
