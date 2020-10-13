7f319559b2fab44eea42485c7859245d6a984614
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-10-13 12:25:07 +0200

    Restructure Completion

--- Front-End/src/Components/UI/Box/Box.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import classes from "./Box.module.css";
-
-const Box = (props) => <div className={classes.Box}>{props.children}</div>;
-export default Box;
--- Front-End/src/store/actions/changeuserphoto.js
+++
@@ -1,8 +0,0 @@
-export const INPUT_BLOB = "INPUT_BLOB";
-
-export const userPictureFileInput = (response) => {
-  return {
-    type: INPUT_BLOB,
-    response,
-  };
-};
