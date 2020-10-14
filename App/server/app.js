4aa3f4eed93fa8d5b1291b46a050e52dc7b2802c
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-10-14 07:54:35 +0200

    Added Linting Packages

--- App/server/app.js
+++ App/server/app.js
@@ -1,5 +1,26 @@
 const http = require("http");
+const bodyParser = require("body-parser");
+const express = require("express");
 
-const server = http.createServer((req, res) => {});
+const app = express();
+const signInRoute = require("./routes/signIn");
+const signOutRoute = require("./routes/signOut");
+const signUpRoute = require("./routes/signUp");
+const deleteLocationRoute = require("./routes/deleteAccountData");
+const deleteAccount = require("./routes/deleteAccount");
+const accountData = require("./routes/accountData");
+const accountSettings = require("./routes/accountSettings");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(signInRoute);
+app.use(signOutRoute);
+app.use(signUpRoute);
+app.use(deleteLocationRoute);
+app.use(deleteAccount);
+app.use(accountData);
+app.use(accountSettings);
+
+const server = http.createServer(app);
 
 server.listen(4030);
