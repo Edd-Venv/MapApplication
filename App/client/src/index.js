f1c3a0ad098d02790abe53755db6b91a3050e2c1
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-10-28 12:14:16 +0100

    BaseUrl Set

--- App/client/src/index.js
+++ App/client/src/index.js
@@ -15,8 +15,8 @@ import changeUserPasswordReducer from "./store/reducers/changeuserpassword";
 import changeUserPhotoReducer from "./store/reducers/changeuserphoto";
 import authReducer from "./store/reducers/auth";
 
-export const BaseUrl = "http://18.222.115.53:4030/";
-//const BaseUrl = "http://localhost:3000"
+//export const BaseUrl = "http://18.222.115.53:4030/";
+export const BaseUrl = "http://localhost:3000";
 
 const rootReducer = combineReducers({
   auth: authReducer,
